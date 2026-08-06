03aa3c5fd676e0990e9def86e4b6454b4e546a99
123abcln <nizhniea@mail.uc.edu>
2026-08-06 13:12:34 -0400

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-for (let i = 0; i < 400; i++) {
+for (let i = 0; i < 500; i++) {
     let star = document.createElement("div");
 
     star.className = "star";
@@ -10,7 +10,9 @@ for (let i = 0; i < 400; i++) {
     star.style.left = x + "px";
     star.style.top = y + "px";
 
-   
+    let size = Math.random() * 6 + 1;
+    star.style.width = size + "px";
+    star.style.height = size + "px";
 
     document.body.append(star);
 
@@ -18,5 +20,5 @@ for (let i = 0; i < 400; i++) {
   let specialStar = document.createElement("div");
   specialStar.className = "special-star";
   specialStar.style.left = window.innerWidth / 2 + "px";
-  specialStar.style.top = window.innerHeight / 2 + "px";   
+  specialStar.style.top = window.innerHeight / 0 + "px";   
   document.body.append(specialStar);
